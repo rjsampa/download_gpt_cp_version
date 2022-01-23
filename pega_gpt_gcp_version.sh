70bc2ada959c2caaf654a7ba0a5aaff49da8b83a
Rafael Sampaio <35374588+rjsampa@users.noreply.github.com>
2022-01-23 17:02:16 -0300

    Add files via upload

---
+++ pega_gpt_gcp_version.sh
@@ -0,0 +1,240 @@
+#/bin/sh
+
+lynx http://gpt.cptec.inpe.br/index.html
+
+caminho1=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/Produtos-Modelos/00Z/gfs/png/
+caminho2=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/Produtos-Modelos/00Z/gfs/figuras/png/
+caminho5=http://wxmaps.org/pix/
+caminho6=http://img0.cptec.inpe.br/~rgptimg/Produtos-Pagina/Carta-Sinotica/Previsao/
+caminho7=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/Produtos-Modelos/00Z/gfs/figuras/png/
+caminho8=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/Produtos-Modelos/00Z/wrf05km/figuras/png/
+caminho9=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/Produtos-Modelos/00Z/gfs/figuras/png/
+caminho10=http://img0.cptec.inpe.br/~rgptimg/Produtos-Pagina/Carta-Sinotica/Analise/
+caminho11=https://s1.cptec.inpe.br/grafico/Modelos/WRF/GHT/meteogramas/PPN/
+
+
+
+
+caminhoA=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/gempak/gfs_sites_discussao_00Z.gif
+caminhoB=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/gempak/gfs_fc06h_00Z.gif
+caminhoC=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/gempak/gfs_sc06h_sup_00Z.gif
+caminhoD=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/gempak/gfs_prec1dia_12Z_100mm.gif
+caminhoE=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/gempak/Azcas_00Z.gif
+caminhoF=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/gempak/Azcas_06Z.gif
+caminhoG=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/gempak/Azcas_12Z.gif
+caminhoH=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/gempak/Azcas_18Z.gif
+caminhoI=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/gempak/Afrentes_00Z.gif
+caminhoJ=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/gempak/Afrentes_06Z.gif
+caminhoK=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/gempak/Afrentes_12Z.gif
+caminhoL=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/gempak/Afrentes_18Z.gif
+caminhoAA=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/gempak/gfs_ventoterm_00Z.gif
+caminhoM=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/12345/fotos/sup00.pdf
+caminhoN=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/12345/fotos/sup06.pdf
+caminhoO=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/12345/fotos/sup12.pdf
+caminhoP=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/12345/fotos/sup18.pdf
+caminhoQ=http://img0.cptec.inpe.br/~rgptimg/Produtos-Pagina/Carta-Sinotica/Analise/impressao/sup_impressao_00.pdf
+caminhoR=http://img0.cptec.inpe.br/~rgptimg/Produtos-Pagina/Carta-Sinotica/Analise/impressao/altitude_impressao_00.pdf
+caminhoS=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/raios/raios_24_BR.gif
+caminhoT=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/raios/raios_24_br.gif
+caminhoU=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/Produtos-Modelos/00Z/acoplado/diagrama1_315.png
+caminhoV=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/Produtos-Modelos/00Z/acoplado/diagrama2_315.png
+caminhoW=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/Produtos-Modelos/00Z/acoplado/diagrama1_315esp.png
+caminhoX=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/Produtos-Modelos/00Z/acoplado/diagrama2_315esp.png
+caminhoY=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/Produtos-Modelos/00Z/acoplado/diagrama1_315_prec.png
+caminhoZ=http://gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/Produtos-Modelos/00Z/acoplado/diagrama2_315_prec.png
+
+
+
+
+wget -U "firefox" -cr ${caminhoA} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoB} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoC} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoD} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoE} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoE} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoF} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoG} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoH} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoI} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoJ} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoK} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoL} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoM} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoN} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoO} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoP} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoQ} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoR} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoS} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoT} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoU} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoV} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoW} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoX} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoY} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoZ} --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminhoAA} --http-user=gpt --http-passwd=gptcpt
+
+i=1
+while [ $i -le 8 ]
+do
+
+wget -U "firefox" -cr ${caminho1}geo_anom_$i.png --http-user=gpt --http-passwd=gptcpt
+
+i=$(( i+ 1))
+
+done
+
+i=1
+while [ $i -le 28 ]
+do
+
+wget -U "firefox" -cr ${caminho2}discussao_jatosistema_$i.png --http-user=gpt --http-passwd=gptcpt
+
+i=$(( i+ 1))
+
+done
+
+
+i=24
+while [ $i -le 144 ]
+do
+
+wget -U "firefox" -cr ${caminho5}sa1.00hr.png --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho5}sa2.00hr.png --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho5}sa3.00hr.png --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho5}sa4.00hr.png --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho5}sa5.00hr.png --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho5}sa6.00hr.png --http-user=gpt --http-passwd=gptcpt
+
+wget -U "firefox" -cr ${caminho5}sa1.${i}hr.png --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho5}sa2.${i}hr.png --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho5}sa3.${i}hr.png --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho5}sa4.${i}hr.png --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho5}sa5.${i}hr.png --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho5}sa6.${i}hr.png --http-user=gpt --http-passwd=gptcpt
+
+i=$(( i+ 24))
+
+done
+
+
+i=24
+while [ $i -le 120 ]
+do
+
+wget -U "firefox" -cr ${caminho6}prev${i}h_atual.gif --http-user=gpt --http-passwd=gptcpt
+
+i=$(( i+ 24))
+
+done
+
+i=0
+while [ $i -le 80 ]
+do
+
+wget -U "firefox" -cr ${caminho7}automatico_mix_${i}.png --http-user=gpt --http-passwd=gptcpt
+
+i=$(( i+ 1))
+
+done
+
+i=0
+while [ $i -le 84 ]
+do
+
+wget -U "firefox" -cr ${caminho8}g1_pslm_prec_${i}.png --http-user=gpt --http-passwd=gptcpt
+
+i=$(( i+ 1))
+
+done
+
+i=0
+while [ $i -le 7 ]
+do
+
+wget -U "firefox" -cr ${caminho2}discussao_zcas_${i}.png --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho2}discussao_500_${i}.png --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho2}discussao_250_${i}.png --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho2}discussao_sup_${i}.png --http-user=gpt --http-passwd=gptcpt
+
+i=$(( i+ 1))
+
+done
+
+
+
+i=0
+while [ $i -le 56 ]
+do
+
+wget -U "firefox" -cr ${caminho9}prev_automatica_zoom_${i}.png --http-user=gpt --http-passwd=gptcpt
+
+i=$(( i+ 1))
+
+done
+
+#data=`date +"%Y%m%d"`
+data=`date -d "1 day ago" '+%Y%m%d'`
+
+wget -U "firefox" -cr ${caminho10}Altitude/altitude_${data}00.gif --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho10}Altitude/altitude_${data}06.gif --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho10}Altitude/altitude_${data}12.gif --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho10}Altitude/altitude_${data}18.gif --http-user=gpt --http-passwd=gptcpt
+
+wget -U "firefox" -cr ${caminho10}Medio/medio_${data}00.gif --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho10}Medio/medio_${data}06.gif --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho10}Medio/medio_${data}12.gif --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho10}Medio/medio_${data}18.gif --http-user=gpt --http-passwd=gptcpt
+
+wget -U "firefox" -cr ${caminho10}Baixo2/700_${data}00.gif --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho10}Baixo2/700_${data}06.gif --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho10}Baixo2/700_${data}12.gif --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho10}Baixo2/700_${data}18.gif --http-user=gpt --http-passwd=gptcpt
+
+wget -U "firefox" -cr ${caminho10}Baixo/baixo_${data}00.gif --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho10}Baixo/baixo_${data}06.gif --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho10}Baixo/baixo_${data}12.gif --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho10}Baixo/baixo_${data}18.gif --http-user=gpt --http-passwd=gptcpt
+
+wget -U "firefox" -cr ${caminho10}Superficie/superficie_${data}00.gif --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho10}Superficie/superficie_${data}06.gif --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho10}Superficie/superficie_${data}12.gif --http-user=gpt --http-passwd=gptcpt
+wget -U "firefox" -cr ${caminho10}Superficie/superficie_${data}18.gif --http-user=gpt --http-passwd=gptcpt
+
+
+i=220
+while [ $i -le 246 ]
+do
+
+wget -U "firefox" -cr ${caminho11}$i.png --http-user=gpt --http-passwd=gptcpt
+
+i=$(( i+ 1))
+
+done
+
+
+
+mkdir ${data}
+cd ${data}
+mkdir figures
+cd ..
+
+
+mv img0.cptec.inpe.br/~rgptimg/Produtos-Pagina/Carta-Sinotica ${data}/
+mv wxmaps.org/pix/*.png* ${data}/figures
+mv gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/gempak ${data}/
+mv gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/Produtos-Modelos/00Z/gfs/figuras/png/*.png* ${data}/figures
+mv gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/Produtos-Modelos/00Z/gfs/png/*.png* ${data}/figures
+mv gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/Produtos-Modelos/00Z/acoplado/*.png* ${data}/figures
+mv gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/raios/*.gif* ${data}/figures
+mv gpt.cptec.inpe.br/~rgpt/pagina/restrito/gptimg/Produtos-Modelos/00Z/wrf05km/figuras/png/*.png* ${data}/figures
+mv gpt.cptec.inpe.br/~rgpt/pagina/restrito/12345/fotos/*.pdf* ${data}/Carta-Sinotica/Analise/impressao
+mv s1.cptec.inpe.br/grafico/Modelos/WRF/GHT/meteogramas/ ${data}/
+
+
+#mv ${data} outputs/
+#cron_backup.bash -v
+
+./gdrive upload --recursive /home/conta_gcp_2/${data}
+
